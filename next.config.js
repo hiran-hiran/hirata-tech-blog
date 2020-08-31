643824ba37e6629f708b943c264634150168acb4
hirata <>
2020-08-31 18:10:19 +0900

    add basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    // basePath: "/",
-    assetPrefix: process.env.GITHUB_PAGES ? '/next-microcms-blog' : '',
+    basePath: process.env.GITHUB_PAGES ? "/next-microcms-blog" : "",
+    assetPrefix: process.env.GITHUB_PAGES ? "/next-microcms-blog" : "",
     env: {
         SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
     }
