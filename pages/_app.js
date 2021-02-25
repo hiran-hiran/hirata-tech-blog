0d80373c3e1e22289e47907af5029dbbb57b3868
hirata <>
2021-02-26 00:29:45 +0900

    add ga tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/global.css'
 import '../styles/common.css'
 import 'highlight.js/styles/night-owl.css'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    Router.events.on('routeChangeComplete', url => gtag.pageview(url))
+  }, [Router.events])
+
 	return <Component {...pageProps} />
 }
 
